6b42fb8efa05b3d760f677150c741637f20b2e21
Hayden Crane <haydenmcrane28@gmail.com>
2025-02-13 00:24:10 -0500

    updated for deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from "@sveltejs/adapter-node";
+import adapter from "@sveltejs/adapter-static";
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,7 +8,13 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter()
+		paths: {
+			base: '',  // Replace 'repo-name' with the name of your repo
+		  },
+		adapter: adapter({
+			// options
+			fallback: null // optional, if you want to handle 404s or dynamic routes
+		  }),
 	}
 };
 
