c08dbd05feb6b1fbf5fc0618267f1bf4f31b3763
Hayden Crane <haydenmcrane28@gmail.com>
2025-02-13 14:24:34 -0500

    updated svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,12 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-auto';
+import preprocess from 'svelte-preprocess';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	preprocess: preprocess(),
 
 	kit: {
 		adapter: adapter(),
