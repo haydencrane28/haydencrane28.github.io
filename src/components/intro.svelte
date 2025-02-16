284064e12d7075a03d2da0814a2c47ffd4b7eafe
Hayden Crane <haydenmcrane28@gmail.com>
2025-02-16 17:29:53 -0500

    rounded icons

--- src/components/intro.svelte
+++ src/components/intro.svelte
@@ -14,15 +14,15 @@
         <p class="mx-auto max-w-[700px] text-sky-100 md:text-xl">
             I thrive at the intersection of software and hardware, where innovation pushes boundaries and turns ideas into reality. The challenge of building seamless, high-performance systems captivates me, and I’m always seeking ways to refine, optimize, and create. Beyond tech, I bring the same drive for precision and mastery to Brazilian Jiu-Jitsu and strength training. I crave intensity—whether it’s on the mat, in the gym, or tearing through snow and dirt on a snowboard or dirt bike. Naturally, this has fueled my love for Red Bull, a brand that embodies fearless ambition. At my core, I’m obsessed with understanding the mind—how we think, adapt, and break limits. That relentless curiosity fuels everything I do.
         </p>
-        <div class="flex mt-4 righ-4 space-x-4">
+        <div class=" flex mt-4 righ-4 space-x-4">
             <a href="https://github.com/haydencrane28" target="_blank" aria-label="GitHub">
-            <Github size={30} class="border text-sky-100 bg-zinc-900" />
+            <Github size={30} class="rounded-sm border text-sky-100 bg-zinc-900" />
             </a>
             <a href="https://www.linkedin.com/in/hayden-crane-compsci" target="_blank" aria-label="LinkedIn">
-            <Linkedin size={30} class="border text-sky-100 bg-zinc-900" />
+            <Linkedin size={30} class="rounded-sm border text-sky-100 bg-zinc-900" />
             </a>
             <a href="mailto:haydenmcrane28@gmail.com" target="_blank" aria-label="Email">
-                <Mail size={30} class="border text-sky-100 bg-zinc-900" />
+                <Mail size={30} class="rounded-sm border text-sky-100 bg-zinc-900" />
             </a>
         </div>
     </div>
